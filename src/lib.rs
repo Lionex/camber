66df40c2689e90bc3ef5e928ea8bc684c514da55
Gwen Lofman <Gwen@Lofman.co>
2017-04-13 18:28:51 -0400

    Add information from README to create docs
    Includes a synopsis of the library and its short-term goals.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
-
+//! > **Camber**: _v._ to curve (an object)
+//!
+//! `Camber` is a curve interpolation library that provides methods to generate a
+//! various types of interpolationg polynomials along with a set of related
+//! utilities developed under MIT license as an educational project.
+//!
+//! As it stands, this library is an exercise to implement the following curve
+//! interpolation methods:
+//!
+//! - Lagrange Divided Difference
+//! - Cubic Curves
+//! - Hermite Curves
+//! - Cubic Bezier Curves
+//! - Bezier Curve truncation
+//! - Catmull-Rom Splines
+//! - Cubic Splines
+//! - B-Splines
+//! - Uniform B Splines
+//! - Non-Uniform B Splines
+//! - Non-Uniform Rational B Splines
 
 #[cfg(test)]
 mod tests {
