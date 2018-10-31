fea91e114cbbac334f75bcc69f25036a3a8da28b
Gwen Lofman <Gwen@Lofman.io>
2018-10-31 17:36:30 -0400

    Re-purpose library for fast 1D transforms
    Rather than curve interpolation, focus on 1D non-linear transform
    functions or "easing" functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,7 @@
 //! > **Camber**: _v._ to curve (an object)
 //!
-//! `Camber` is a curve interpolation library that provides methods to generate
-//! various types of interpolationg polynomials along with a set of related
-//! utilities developed under MIT license as an educational project.
-//!
-//! As it stands, this library is an exercise to implement the following curve
-//! interpolation methods:
-//!
-//! - Lagrange Divided Difference
-//! - Cubic Curves
-//! - Hermite Curves
-//! - Cubic Bezier Curves
-//! - Bezier Curve truncation
-//! - Catmull-Rom Splines
-//! - Cubic Splines
-//! - B-Splines
-//! - Uniform B Splines
-//! - Non-Uniform B Splines
-//! - Non-Uniform Rational B Splines
+//! `Camber` is a library of fast, composeable, non-linear, 1D transformation functions also known
+//! as _easing functions_ or _normalized utility functions_.
 
 mod utility;
 
