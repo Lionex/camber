36ba71f86c6b2134597ffe9da38974158e2b3ff5
Gwen Lofman <Gwen@Lofman.io>
2018-11-18 11:44:41 -0500

    Add example output for poly_eval

---
+++ examples/plot.rs
@@ -0,0 +1,34 @@
+extern crate plotlib;
+extern crate camber;
+
+use plotlib::style::Line;
+use plotlib::page::Page;
+use plotlib::view::View;
+use plotlib::function::{ Function, Style };
+use camber::utility::poly_eval;
+
+fn main() {
+    let linear = [1., 0.];
+    let quadratic = [1., 0., 0.];
+    let cubic = [1., 0., 0., 0.];
+
+    let linear = Function::new(|x| poly_eval(&linear, x), -1., 1.)
+        .style(Style::new().colour("red"));
+    let quadratic = Function::new(|x| poly_eval(&quadratic, x), -1., 1.)
+        .style(Style::new().colour("green"));
+    let cubic = Function::new(|x| poly_eval(&cubic, x), -1., 1.)
+        .style(Style::new().colour("blue"));
+
+    let v = View::new()
+        .add(&linear)
+        .add(&quadratic)
+        .add(&cubic);
+    Page::single(&v).save("polyeval_1.svg");
+
+    let complicated = [-2., -1., 1., -0.1];
+    let complicated = Function::new(|x| poly_eval(&complicated, x), -1., 1.)
+        .style(Style::new().colour("blue"));
+    let v = View::new().add(&complicated);
+    Page::single(&v).save("polyeval_2.svg");
+
+}
