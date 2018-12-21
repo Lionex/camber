4250fd3a349dd119f876484d62c617d0ebcdbad8
Gwen Lofman <Gwen@Lofman.io>
2018-12-21 15:11:06 -0500

    Create one function per example plot

--- examples/plot.rs
+++ examples/plot.rs
@@ -7,7 +7,12 @@ use plotlib::view::View;
 use plotlib::function::{ Function, Style };
 use camber::poly_eval;
 
-fn main() {
+/// `poly_eval` can be used to create simple polynomials from their coefficients
+///
+/// Can calculate each value in `O(n)` multiplications, for `n` coefficients, or `n`-1 degree
+/// polynomials.
+fn poly_eval_demo() {
+    // Plot linear, cubic, and quartic equations together
     let linear = [1., 0.];
     let quadratic = [1., 0., 0.];
     let cubic = [1., 0., 0., 0.];
@@ -27,8 +32,12 @@ fn main() {
 
     let complicated = [-2., -1., 1., -0.1];
     let complicated = Function::new(|x| poly_eval(&complicated, x), -1., 1.)
-        .style(Style::new().colour("blue"));
+    .style(Style::new().colour("blue"));
     let v = View::new().add(&complicated);
     Page::single(&v).save("polyeval_2.svg");
+}
+
+fn main() {
+    poly_eval_demo();
 
 }
