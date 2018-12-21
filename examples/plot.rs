54528283a4343b16bfafabc692f05e9118a37ea9
Gwen Lofman <Gwen@Lofman.io>
2018-12-21 15:31:28 -0500

    Add examples for all the easing functions

--- examples/plot.rs
+++ examples/plot.rs
@@ -1,3 +1,7 @@
+/*!
+Plot easing functions to show examples of their capabilities
+ */
+
 extern crate plotlib;
 extern crate camber;
 
@@ -33,12 +37,63 @@ fn poly_eval_demo() {
 
     let complicated = [-2., -1., 1., -0.1];
     let complicated = Function::new(|x| poly_eval(&complicated, x), -1., 1.)
-    .style(Style::new().colour("blue"));
+        .style(Style::new().colour("blue"));
     let v = View::new().add(&complicated);
     Page::single(&v).save("img/polyeval_2.svg");
+}
+
+macro_rules! plot {
+    ($($f:ident),+) => {
+        $(
+            // Define the function to plot from the given identifier
+            let f = Function::new(|t| $f(t), 0., 1.)
+            .style(Style::new().colour("red"));
+
+            // Add the function to the view
+            let v = View::new().add(&f);
+
+            // Write out to a file
+            Page::single(&v).save(concat!("img/", stringify!($f), ".svg"));
+        )+
+    }
 }
 
 fn main() {
     poly_eval_demo();
 
+    // Plot and save the smooth start functions
+    plot! {
+        smooth_start_2,
+        smooth_start_3,
+        smooth_start_4,
+        smooth_start_5,
+        smooth_start_6,
+        smooth_start_7,
+        smooth_start_8,
+        smooth_start_9
+    }
+
+    // Plot and save the smooth stop functions
+    plot! {
+        smooth_stop_2,
+        smooth_stop_3,
+        smooth_stop_4,
+        smooth_stop_5,
+        smooth_stop_6,
+        smooth_stop_7,
+        smooth_stop_8,
+        smooth_stop_9
+    }
+
+    // Plot and save the smooth step functions
+    plot! {
+        smooth_step_2,
+        smooth_step_3,
+        smooth_step_4,
+        smooth_step_5,
+        smooth_step_6,
+        smooth_step_7,
+        smooth_step_8,
+        smooth_step_9
+    }
 }
